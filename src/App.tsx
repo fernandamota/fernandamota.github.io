2ab0841e44911c267412c758363d58177ee7c356
Filipe Aguiar <filipe.aguiar@ufpe.br>
2025-06-28 10:19:45 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContractForm from './components/ContractForm';
 import ClientForm from './components/ClientForm';
 import Portfolio from './components/Portfolio';
+import MetaTagUpdater from './components/MetaTagUpdater';
 
 function App() {
-  const location = useLocation();
-
   useEffect(() => {
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     document.documentElement.setAttribute('data-bs-theme', prefersDarkMode ? 'dark' : 'light');
@@ -16,14 +15,9 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    const currentUrl = window.location.origin + location.pathname;
-    document.querySelector('meta[property="og:url"]')?.setAttribute('content', currentUrl);
-    document.querySelector('meta[name="twitter:url"]')?.setAttribute('content', currentUrl);
-  }, [location.pathname]);
-
   return (
     <Router basename="/">
+      <MetaTagUpdater />
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/gerar-contrato" element={<ContractForm />} />
